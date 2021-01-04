91bda108d5e4b3e1dae212ccf8d935fd18d15ec6
Christian Mena <christian.91.gm@hotmail.com>
2021-01-04 16:44:52 -0500

    Created Login View

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -5,9 +5,151 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      body: Stack(
+        children: <Widget>[
+          _createBackgroud(context),
+          _formLogin(context)
+        ],
       ),
+    );
+  }
+
+  Widget _createBackgroud(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final backgroudColor =  Container(
+      height: size.height*0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color>[
+            Color.fromRGBO(63, 63, 156, 1.0),
+            Color.fromRGBO(90, 70, 178, 1.0)
+          ]
+        )
+      ),
+    );
+
+    final circle = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05)
+      ),
+    );
+
+    final iconText = Container(
+      padding: EdgeInsets.only(top: 80.0),
+      child: Column(
+        children: <Widget>[
+          Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
+          SizedBox(height: 10.0, width: double.infinity,),
+          Text('StkngEsk', style: TextStyle( color: Colors.white, fontSize: 25.0))
+        ],
+      ),
+    );
+
+
+    return Stack(
+      children: <Widget>[
+        backgroudColor,
+        Positioned(top: 90.0, left: 30.0, child: circle),
+        Positioned(top: -40.0, right: -30.0, child: circle),
+        Positioned(bottom: -50.0, right: -10.0, child: circle),
+        Positioned(bottom: 120.0, right: 20.0, child: circle),
+        Positioned(bottom: -50.0, left: -20.0, child: circle),
+        iconText
+      ],
+    );
+  }
+
+  Widget _formLogin(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+          Container(
+            width: size.width*0.85,
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            margin: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0
+                )
+              ]
+            ),
+            child: Column(
+              children: <Widget>[
+                Text('Login', style: TextStyle(fontSize: 20.0)),
+                SizedBox(height: 60.0),
+                _createEmail(),
+                SizedBox(height: 30.0),
+                _createPassword(),
+                SizedBox(height: 30.0),
+                _createButton()
+              ],
+            ),
+          ),
+          Text('Forgot your password?'),
+          SizedBox(height: 100.0)
+        ],
+      ),
+    );
+  }
+
+  Widget _createEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          hintText: 'mail@example.com',
+          labelText: 'Email'
+        ),
+      ),
+    );
+  }
+
+  Widget _createPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+          labelText: 'Password'
+        ),
+      ),
+    );
+  }
+
+  Widget _createButton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 15.0),
+        child: Text('Sign in'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: () {},
     );
   }
 }
